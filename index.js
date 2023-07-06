be759e45ea980492d2d3776cfafc6436928a87c0
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-07-06 17:45:37 +0200

    Completed lab

--- index.js
+++ index.js
@@ -15,9 +15,66 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+  // Iteration 2
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = Recipe.create({
+      title: "Curry Sausage",
+      level: 'Easy Peasy',
+      ingredients: ["sausage", "ketchup", "pommes", "curry",],
+      cuisine: "German",
+      dishType: "main_course",
+      duration: 1,
+      creator: "Alex",
   })
+  })
+
+  // Iteration 3
+  .then( () => {
+    return Recipe.insertMany(data)
+  })
+
+  .then( () => {
+    console.log(Recipe.title)
+  })
+
+  // Iteration 4
+
+  .then( () => {
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, // Search condition
+    { $set: { duration: 100 } }, // Update operation
+    { new: true } )
+  })
+
+  .then( () => {
+    console.log('duration updated succsesfully')
+  })
+
+  // Iteration 5
+
+  .then(() => {
+    return Recipe.deleteOne({
+      title: "Carrot Cake"
+    })
+  })
+  
+  .then(() => {
+    console.log("Carrot Cake recipe deleted successfully!")
+  })
+
+  // Iteration 6
+
+  .finally( () => {
+    return mongoose.connection.close();
+  })
+
+  .then( () => {
+    console.log('connection closed');
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+   
